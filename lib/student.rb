0481b7d3b7a2833c528696fe17ed85d8ba34c557
Nathan Mousa <nathanmousa1@gmail.com>
2019-12-17 16:33:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,7 +9,14 @@ class Student
   end
   
   def self.create_table
-    
+    table =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        album TEXT
+        )
+        SQL
+    DB[:conn].execute(table) 
   end
   
 end
